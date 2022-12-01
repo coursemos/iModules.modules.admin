5a667bcfbc65a00be5dab86b3a0b92b11a618c0b
arzzcom <arzz@arzz.com>
2022-12-01 13:24:01 +0900

    관리자 UI 컴포넌트 - 버튼 클래스 추가

---
+++ scripts/Admin.Button.js
@@ -0,0 +1,78 @@
+/**
+ * 이 파일은 아이모듈 관리자모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 버튼 클래스를 정의한다.
+ *
+ * @file /modules/admin/scripts/Admin.Button.ts
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2022. 12. 1.
+ */
+var Admin;
+(function (Admin) {
+    class Button extends Admin.Component {
+        type = 'button';
+        role = 'button';
+        text;
+        iconClass;
+        toggle;
+        pressed;
+        handler;
+        $button;
+        /**
+         * 버튼을 생성한다.
+         *
+         * @param {Object} properties - 객체설정
+         */
+        constructor(properties = null) {
+            super(properties);
+            this.text = this.properties.text ?? '';
+            this.iconClass = this.properties.iconClass ?? null;
+            this.toggle = this.properties.toggle ?? false;
+            this.pressed = false;
+            this.handler = this.properties.handler ?? null;
+            this.$button ??= Html.create('button').setAttr('type', 'button');
+        }
+        /**
+         * 레이아웃을 렌더링한다.
+         */
+        render() {
+            if (this.iconClass !== null) {
+                let $icon = Html.create('i').addClass('icon');
+                $icon.addClass(...this.iconClass.split(' '));
+                this.$button.append($icon);
+            }
+            if (this.text !== null) {
+                let $text = Html.create('span').text(this.text);
+                this.$button.append($text);
+            }
+            if (this.handler !== null || this.toggle === true) {
+                this.$button.on('click', () => {
+                    if (this.toggle == true) {
+                        this.setPressed(!this.pressed);
+                    }
+                    if (this.handler !== null) {
+                        this.handler(this);
+                    }
+                });
+            }
+            this.append(this.$button);
+            super.render();
+        }
+        /**
+         * 토글 상태를 변경한다.
+         *
+         * @param {boolean} toggle - 토글여부
+         */
+        setPressed(toggle) {
+            this.pressed = toggle;
+            if (this.pressed == true) {
+                this.$button.addClass('pressed');
+            }
+            else {
+                this.$button.removeClass('pressed');
+            }
+        }
+    }
+    Admin.Button = Button;
+})(Admin || (Admin = {}));
