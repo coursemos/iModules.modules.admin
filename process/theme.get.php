024d75807e969e053d67cfe70efb63f2657329b1
arzzcom <arzz@arzz.com>
2023-03-21 22:27:54 +0900

    테마 환경설정을 가져오는 프로세스 추가

---
+++ process/theme.get.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 이 파일은 아이모듈 관리자모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 사이트 테마 정보를 가져온다.
+ *
+ * @file /modules/admin/process/theme.get.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2023. 3. 20.
+ */
+if (defined('__IM_PROCESS__') == false) {
+    exit();
+}
+
+/**
+ * 관리자권한이 존재하는지 확인한다.
+ * @var \modules\admin\Admin $mAdmin
+ */
+$mAdmin = Modules::get('admin');
+if ($mAdmin->checkPermission('/sites') == false) {
+    $results->success = false;
+    $results->message = $mAdmin->getErrorText('FORBIDDEN');
+    return;
+}
+
+$name = Request::get('name', true);
+$theme = new Theme((object) ['name' => $name]);
+$results->success = true;
+$results->theme = $name;
+$results->fields = $theme->getPackage()->getConfigFields();
