c7e57c5e83a35ce1e51ca9590c77a508440b653d
arzzcom <arzz@arzz.com>
2023-03-21 22:27:46 +0900

    템플릿 목록을 가져오는 프로세스 추가

---
+++ process/themes.get.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * 이 파일은 아이모듈 관리자모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 사이트 테마목록을 가져온다.
+ *
+ * @file /modules/admin/process/themes.get.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2023. 3. 20.
+ */
+if (defined('__IM_PROCESS__') == false) {
+    exit();
+}
+
+/**
+ * 관리자권한이 존재하는지 확인한다.
+ * @var \modules\admin\Admin $mAdmin
+ */
+$mAdmin = Modules::get('admin');
+if ($mAdmin->checkPermission('/sites') == false) {
+    $results->success = false;
+    $results->message = $mAdmin->getErrorText('FORBIDDEN');
+    return;
+}
+
+$themes = [];
+$names = File::getDirectoryItems(Configs::path() . '/themes', 'directory', false);
+foreach ($names as $name) {
+    $themes[] = new Theme((object) ['name' => basename($name)]);
+}
+
+$modules = Modules::all();
+foreach ($modules as $module) {
+    if ($module->is_theme == true) {
+        $mModule = Modules::get($module->name);
+        $names = File::getDirectoryItems($mModule->getPath() . '/themes', 'directory', false);
+        foreach ($names as $name) {
+            $themes[] = new Theme((object) ['name' => '/modules/' . $module->name . '/' . basename($name)]);
+        }
+    }
+}
+
+$records = [];
+foreach ($themes as $theme) {
+    $records[] = [
+        'name' => $theme->getName(false),
+        'title' => $theme->getTitle(),
+        'dir' => $theme->getBase(),
+        'screenshot' => $mAdmin->getDir() . '/images/noimage.png',
+    ];
+}
+
+$results->success = true;
+$results->records = $records;
