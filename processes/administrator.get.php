9ca02b8bdafbb3ca38a1478f3ba384c6be89ecd4
arzzcom <arzz@arzz.com>
2024-01-28 18:02:32 +0900

    관리자 정보 가져오기 프로세스 추가

---
+++ processes/administrator.get.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 이 파일은 아이모듈 관리자모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 컨텍스트 정보를 가져온다.
+ *
+ * @file /modules/admin/processes/context.get.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2024. 1. 26.
+ *
+ * @var \modules\admin\Admin $me
+ */
+if (defined('__IM_PROCESS__') == false) {
+    exit();
+}
+
+/**
+ * 관리자권한이 존재하는지 확인한다.
+ */
+if (false && $me->getAdmin()->checkPermission('administrators', ['edit']) == false) {
+    $results->success = false;
+    $results->message = $me->getErrorText('FORBIDDEN');
+    return;
+}
+
+$member_id = Request::get('member_id', true);
+$administrator = $me->getAdmin()->getAdministrator($member_id);
+if ($administrator == null) {
+    $results->success = false;
+    $results->message = $me->getErrorText('NOT_FOUND_DATA');
+    return;
+}
+
+$permission = new \modules\admin\dtos\Permission();
+
+$results->success = true;
+$results->groups = [];
+foreach ($administrator->getGroups() as $group) {
+    $results->groups[] = $group->getGroup()->getJson();
+    $permission->addPermissions(
+        $group
+            ->getGroup()
+            ->getPermission()
+            ->getPermissions()
+    );
+}
+
+$results->member_id = $administrator->getId();
+$results->permissions = $administrator->getPermission()->getPermissions();
+$results->group_permissions = $permission->getPermissions();
