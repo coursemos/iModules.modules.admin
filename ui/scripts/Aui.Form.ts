4b42493a969c0a83931ab6e64045f688f4a8dc32
arzzcom <arzz@arzz.com>
2024-09-19 17:14:10 +0900

    원본 데이터 확인

--- ui/scripts/Aui.Form.ts
+++ ui/scripts/Aui.Form.ts
@@ -882,7 +882,9 @@ namespace Aui {
                     this.value = value;
 
                     if (Format.isEqual(value, this.pValue) == false) {
-                        this.onChange();
+                        if (is_origin == false) {
+                            this.onChange();
+                        }
                         this.pValue = value;
                     }
 
